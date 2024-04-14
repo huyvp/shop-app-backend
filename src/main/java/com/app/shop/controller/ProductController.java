39eafb133151b9fad9b5d8c69afad6897d13c5c3
huyn29 <nvh1892kw@gmail.com>
2024-04-14 22:59:43 +0700

    [shop-app] Add banner custom

--- src/main/java/com/app/shop/controller/ProductController.java
+++ src/main/java/com/app/shop/controller/ProductController.java
@@ -2,30 +2,29 @@ package com.app.shop.controller;
 
 import com.app.shop.dto.ProductDTO;
 import jakarta.validation.Valid;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("api/v1/products")
 public class ProductController {
     @GetMapping("")
-    public String getAllProduct(@RequestParam("page") int page, @RequestParam("limit") int limit){
+    public String getAllProduct(@RequestParam("page") int page, @RequestParam("limit") int limit) {
         return "Get all product";
     }
+
     @GetMapping("/{id}")
-    public String getProductById(@PathVariable int id){
+    public String getProductById(@PathVariable int id) {
         return "Product by id";
     }
+
     @PostMapping("")
-    public String createProduct(@RequestBody @Valid ProductDTO productDTO){
+    public String createProduct(@RequestBody @Valid ProductDTO productDTO, @RequestPart("file") MultipartFile file) {
         return "Create Product";
     }
+
     @DeleteMapping("/{id}")
-    public String deleteProduct(@PathVariable int id){
+    public String deleteProduct(@PathVariable int id) {
         return "Delete Product";
     }
 }
